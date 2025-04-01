79b49770466aec860862697a1492e91c89aeb33e
toni-zitoni <devin.winters77@gmail.com>
2025-03-31 22:27:55 -0600

    Added tests/integration/test_types.py.

---
+++ Task_manager_app/tests/integration/test_types.py
@@ -0,0 +1,18 @@
+from fastapi import APIRouter
+from Task_manager_app.src.JSONManager import *
+import requests
+from Task_manager_app.src.TaskManager import *
+import unittest
+
+BASE_URL = "http://127.0.0.1:8000"  # Update with your FastAPI server URL if different
+
+class TestTaskManager(unittest.TestCase):
+    def __init__(self, methodName = "runTest"):
+        super().__init__(methodName)
+        self.task_manager = TaskManager(JSONManager("http://localhost:8000/tasks", "data/test.json"), APIRouter())
+
+    def test_save_task(self):
+        self.assertRaises(TypeError, self.task_manager.save_task(12))
+
+    def test_update_task(self):
+        self.assertRaises(TypeError, self.task_manager.update_task("abc", 12))
