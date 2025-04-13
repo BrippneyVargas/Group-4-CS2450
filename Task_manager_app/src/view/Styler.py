5e9ad39cc1d322549509ebe1606f65b4e71162a6
toni-zitoni <devin.winters77@gmail.com>
2025-04-12 23:30:24 -0600

    Implemented config.ini and changed Styler to use config.ini instead of ThemeColor.py

--- Task_manager_app/src/view/Styler.py
+++ Task_manager_app/src/view/Styler.py
@@ -1,8 +1,17 @@
+from configparser import ConfigParser
 import streamlit as st
 from ThemeColor import *
+import os
 
 class Styler:
     """Apply custom CSS styles for the application."""
+    config_object = ConfigParser()
+
+    @staticmethod
+    def refresh():
+        os.chdir(os.path.join("Task_manager_app", "src"))
+        Styler.config_object.read("config.ini")
+        os.chdir(os.path.join("..", ".."))
 
     @staticmethod
     def apply_custom_theme(dark_mode: bool): #Change the two modes to a composition.
@@ -20,48 +29,51 @@ class Styler:
             - If streamlit package is not installed, there'll be an error: ModuleNotFoundError: No module named 'streamlit'
 
         """
+        Styler.refresh()
+        priority_info = Styler.config_object["PRIORITY"]
+        theme_info = None
         if dark_mode: 
+            theme_info = Styler.config_object["DARKMODE"]
             darkmode_css = f"""
             <style>
             body, .stApp {{ overflow: hidden !important; max-height: 100vh; }}
-            .stApp {{ background-color: {ThemeColor.BACKGROUND_DARK}; max-width: 100vw !important; margin: 0; padding: 0; }}
+            .stApp {{ background-color: {theme_info["background_color"]}; max-width: 100vw !important; margin: 0; padding: 0; }}
             ::-webkit-scrollbar {{ display: none; }}
-            .priority-high {{ background-color: {ThemeColor.HIGH_PRIORITY}; color: white; padding: 5px 10px; border-radius: 5px; text-align: center; }}
-            .priority-medium {{ background-color: {ThemeColor.MEDIUM_PRIORITY}; color: white; padding: 5px 10px; border-radius: 5px; text-align: center; }}
-            .priority-low {{ background-color: {ThemeColor.LOW_PRIORITY}; color: black; padding: 5px 10px; border-radius: 5px; text-align: center; }}
-            .footer {{ position: fixed; left: 0; bottom: 0; width: 100%; text-align: center; padding: 20px 0; color: {ThemeColor.PRIMARY_LIGHT}; font-size: 0.9em; background-color: {ThemeColor.BACKGROUND_DARK}; }}
-            .app-title {{ color: {ThemeColor.PRIMARY_LIGHT}; text-align: center; font-size: 3.5em; margin-bottom: 20px; font-weight: bold; }}
+            .priority-high {{ background-color: {priority_info["high"]}; color: white; padding: 5px 10px; border-radius: 5px; text-align: center; }}
+            .priority-medium {{ background-color: {priority_info["medium"]}; color: white; padding: 5px 10px; border-radius: 5px; text-align: center; }}
+            .priority-low {{ background-color: {priority_info["low"]}; color: black; padding: 5px 10px; border-radius: 5px; text-align: center; }}
+            .footer {{ position: fixed; left: 0; bottom: 0; width: 100%; text-align: center; padding: 20px 0; color: {theme_info["primary_light"]}; font-size: 0.9em; background-color: {theme_info["background_color"]}; }}
+            .app-title {{ color: {theme_info["primary_light"]}; text-align: center; font-size: 3.5em; margin-bottom: 20px; font-weight: bold; }}
             </style>
             """
             st.markdown(darkmode_css, unsafe_allow_html=True)
 
         else: 
+            theme_info = Styler.config_object["LIGHTMODE"]
             lightmode_css = f"""
             <style>
-            
             .stRadio label[for="High"] {{ color: black !important;}}
             body, .stApp {{ overflow: hidden !important; max-height: 100vh; }}
-            .stApp {{ background-color: {ThemeColor.BACKGROUND_LIGHT}; max-width: 100%; max-height: 100%; margin: 0 auto; padding: 0 20px; color: {ThemeColor.TEXT_COLOR}; }}
+            .stApp {{ background-color: {theme_info['background_color']}; max-width: 100%; max-height: 100%; margin: 0 auto; padding: 0 20px; color: {ThemeColor.TEXT_COLOR}; }}
             ::-webkit-scrollbar {{ display: none; }}
-            div.stTextInput label {{ background-color: {ThemeColor.BACKGROUND_LIGHT}; color: {ThemeColor.TEXT_COLOR}; }}
+            div.stTextInput label {{ background-color: {theme_info['background_color']}; color: {theme_info['text_color']}; }}
             div.stTextInput input {{background-color: white; color: black}}
-            div.stSelectbox label {{ background-color: {ThemeColor.BACKGROUND_LIGHT}; color: {ThemeColor.TEXT_COLOR}; }}
+            div.stSelectbox label {{ background-color: {theme_info['background_color']}; color: {theme_info['text_color']}; }}
             div[data-baseweb="select"] > div {{background-color: white; color: black}}
             #root > div:nth-child(n) > div > div > div > div > div > div > ul li[aria-selected="true"] {{background-color: #FF99FF; color: black !important;}}
             #root > div:nth-child(2) > div > div > div > div > div > div > ul {{background-color: white;}}
             #root > div:nth-child(2) > div > div > div > div > div > div > ul div {{color: black;}}
             #root > div:nth-child(2) > div > div > div > div > div > div > ul li:hover {{background-color: #FFCCFF;}}
             div[data-baseweb="select"] svg[data-baseweb="icon"] {{fill: #FF99FF;}}
-            div.stTextArea label {{ background-color: {ThemeColor.BACKGROUND_LIGHT}; color: {ThemeColor.TEXT_COLOR}; }}
+            div.stTextArea label {{ background-color: {theme_info['background_color']}; color: {theme_info['text_color']}; }}
             div.stTextArea textarea {{background-color: white; color: black}}
-            div.stRadio > label {{ background-color: {ThemeColor.BACKGROUND_LIGHT}; color: {ThemeColor.TEXT_COLOR}; }}     
-            .priority-high {{ background-color: {ThemeColor.HIGH_PRIORITY}; color: black; padding: 5px 10px; border-radius: 5px; text-align: center; }}
-            .priority-medium {{ background-color: {ThemeColor.MEDIUM_PRIORITY}; color: black; padding: 5px 10px; border-radius: 5px; text-align: center; }}
-            .priority-low {{ background-color: {ThemeColor.LOW_PRIORITY}; color: black; padding: 5px 10px; border-radius: 5px; text-align: center; }}
-            .stButton > button {{ background-color: {ThemeColor.BUTTON_COLOR}; color: black; padding: 5px 10px; border-radius: 5px; text-align: center; }}
-            .footer {{ position: fixed; left: 0; bottom: 0; width: 100%; text-align: center; padding: 20px 0; color: {ThemeColor.PRIMARY_LIGHT_LM}; font-size: 0.9em; background-color: {ThemeColor.BACKGROUND_LIGHT}; }}
-            .app-title {{ color: {ThemeColor.PRIMARY_LIGHT_LM}; text-align: center; font-size: 3.5em; margin-bottom: 20px; font-weight: bold; }}
-            
+            div.stRadio > label {{ background-color: {theme_info['background_color']}; color: {theme_info['text_color']}; }}     
+            .priority-high {{ background-color: {priority_info['high']}; color: black; padding: 5px 10px; border-radius: 5px; text-align: center; }}
+            .priority-medium {{ background-color: {priority_info['medium']}; color: black; padding: 5px 10px; border-radius: 5px; text-align: center; }}
+            .priority-low {{ background-color: {priority_info['low']}; color: black; padding: 5px 10px; border-radius: 5px; text-align: center; }}
+            .stButton > button {{ background-color: {theme_info['button_color']}; color: black; padding: 5px 10px; border-radius: 5px; text-align: center; }}
+            .footer {{ position: fixed; left: 0; bottom: 0; width: 100%; text-align: center; padding: 20px 0; color: {theme_info['primary_light']}; font-size: 0.9em; background-color: {theme_info['background_color']}; }}
+            .app-title {{ color: {theme_info['primary_light']}; text-align: center; font-size: 3.5em; margin-bottom: 20px; font-weight: bold; }}
             </style>
             """
             st.markdown(lightmode_css, unsafe_allow_html=True)
